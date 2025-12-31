a9dd7d5daf66661f3b2636092d8a2f64e7ad846b
priyanshuraj-dev <priyanshuraj979837@gmail.com>
2025-12-31 22:46:22 +0530

    Update favicon and name of projet and remove extra space from username

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -1,7 +1,8 @@
 import {z} from "zod";
 
 export const signupSchema = z.object({
-    username: z.string().min(3,"Username must be atleast 3 char").max(20,"Username must be no more than 20 char"),
+    username: z.string().min(3,"Username must be atleast 3 char").max(20,"Username must be no more than 20 char").refine((value) => !/\s/.test(value),
+         "Username must not contain spaces"),
     email: z.string().email({message:"Invalid email address"}),
     password: z.string().min(6,{message:"Password must be atleast 6 char"})
 })
